351232a6bd1c3de33361693fb4ef59ab025bcbcf
jfpiovesa <78732629+jfpiovesa@users.noreply.github.com>
2021-04-22 22:20:50 -0300

    atualizaçao
    station

---
+++ Assets/Script/AgenteManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgenteManager : MonoBehaviour
+{ 
+    GameObject[] agents;// fazendo um arrey/lista de objtos 
+   void Start () 
+   {
+        agents= GameObject.FindGameObjectsWithTag("AI");// pegando os objetos que tem a tag "Ai" em cena e pondo dentro do Arrey/Lista quando se inicia a cena 
+   }
+    // Update is called once per framevoid
+   void Update () 
+   {
+        if(Input.GetMouseButtonDown(0)) // se clicar o botão do mouse 0 faça uma ação
+        {
+            RaycastHit hit;
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 200))// pegando a posiçãoda camera em uma certa distacia, e si a posição do mouse estiver na vissão da camera e colidir com objeto com colisão 
+                {
+                    foreach(GameObject a in agents)//verifica os objetos que estão dentro do arrey/lista e manda um ação 
+                    a.GetComponent<AiControl>().agent.SetDestination(hit.point);//se componete AiControl tiver no objeto, mande ele seguir uma posição que sera a posição do mouse que é clicada,  passada anterior mente. 
+            
+                }
+        }
+    }
+}
---
+++ Assets/Script/AiControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiControl : MonoBehaviour
+{
+      public NavMeshAgent agent; // declarando  como publico o componete 
+        // Use this for initialization
+      void  Start ()
+      {
+        agent= this.GetComponent<NavMeshAgent>(); // pegando componete que em no objeto que este scripte sera posto
+    }
+}
