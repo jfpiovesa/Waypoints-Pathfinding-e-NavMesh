3d3d7144a05d8d63c198debcdd29992502163472
jfpiovesa <78732629+jfpiovesa@users.noreply.github.com>
2021-05-01 23:39:14 -0300

    fish
    nova tafera do peixe

---
+++ Assets/Script/FLOCK.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FLOCK : MonoBehaviour
+{
+    public FlockManager myManager;// pegando o scrip 
+    float speed;
+
+    
+    void Start()
+    {
+        speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);// definendo uma velocidade ao objeto apartir do scripte flockmanager que a uma velocidade definida pelo usuario
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, Time.deltaTime * speed);//movimentando o objeto para frente 
+    }
+}
---
+++ Assets/Script/FlockManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public GameObject fishPrefab; // local que o prefab do objeto que sera criado  na cena 
+    public int numFish = 20; // numero de objetos que vãos ser criados
+    public GameObject[] allFish;// arrey de objetos 
+    public Vector3 swinLimits = new Vector3(5, 5, 5);
+
+    [Header("Configurações do Cardume")]
+    [Range(0.0f, 5.0f)]// tamanho da velocidade minima 
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]// tamanho da velocidade maxima 
+    public float maxSpeed;
+    private void Start()
+    {
+        allFish = new GameObject[numFish]; // pondo todos os objetos em um arrey
+        for(int i = 0; i < numFish; i++)//pasando em cada objeto para poder spawnar em posições diferentes
+        {
+            Vector3 pos = this.transform.position + new Vector3(
+                Random.Range(-swinLimits.x, swinLimits.x), 
+                Random.Range(-swinLimits.y, swinLimits.y),
+                Random.Range(-swinLimits.z, swinLimits.z));
+            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity); 
+        }
+    }
+}
